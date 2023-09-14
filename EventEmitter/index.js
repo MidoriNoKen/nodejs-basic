bcac8e14285bb4559ed8a3453a69ecb0fc5dc2a8
MidoriNoKen <midor1nok3n@gmail.com>
2023-09-14 10:01:18 +0700

    EventEmitter

--- EventEmitter/index.js
+++ EventEmitter/index.js
@@ -5,17 +5,27 @@ const myEventEmitter = new EventEmitter()
 
 // Fungsi yang akan dijalankan ketika event coffee-order terjadi
 
-const makeCoffee = ({ name }) => {
+// const makeCoffee = ({ name }) => {
+const makeCoffee = (name) => {
     console.log(`Kopi ${name} telah dibuat!`)
 }
 
-const makeBill = ({ price }) => {
+// const makeBill = ({ price }) => {
+const makeBill = (price) => {
     console.log(`Bill sebesar ${price} telah dibuat!`)
 }
+
 // Mendaftarkan fungsi makeCoffee sebagai listener event coffee-order
 
-myEventEmitter.on('coffee-order', makeCoffee)
-myEventEmitter.on('coffee-order', makeBill)
+const onCoffeeOrderedListener = ({ name, price }) => {
+    makeCoffee(name)
+    makeBill(price)
+}
+
+// myEventEmitter.on('coffee-order', makeCoffee)
+// myEventEmitter.on('coffee-order', makeBill)
+
+myEventEmitter.on('coffee-order', onCoffeeOrderedListener)
 
 // Memicu event 'coffee-order' terjadi.
 
